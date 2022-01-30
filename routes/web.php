25c2c64d58818dcd02633c691eec0cac53c5442e
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-01-30 15:33:31 +0700

    midtrans_fix

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\midtransController;
 use App\Http\Controllers\rajaongkirController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('province',[rajaongkirController::class,'initProvince'])->name('province');
-Route::get('city/{id}',[rajaongkirController::class,'initCity'])->name('city');
+Route::get('/province',[rajaongkirController::class,'initProvince'])->name('province');
+Route::get('/city/{id}',[rajaongkirController::class,'initCity'])->name('city');
 Route::get('/origin={city_origin}&destination={city_destination}&weight={weight}&courier={courier}',[rajaongkirController::class,'initCost']);
+Route::get('/bayar',[midtransController::class,'paymentgateway']);
