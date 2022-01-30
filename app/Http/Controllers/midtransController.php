16dc3a555117fb3ec219565d7d76b77c92a9f8c8
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-01-30 15:33:44 +0700

    fixxx

--- app/Http/Controllers/midtransController.php
+++ app/Http/Controllers/midtransController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class midtransController extends Controller
 {
-    //midtrans bugs
-    //  public function paymentgatway()
-    // {
-
-    //     // Set your Merchant Server Key
-    //     Veritrans_Config::$serverKey = 'SB-Mid-server-eUV-IAeZn_aXDtqQGyyj4jea';
-    //     // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
-    //     Veritrans_Config::$isProduction = false;
-    //     // Set sanitization on (default)
-    //     Veritrans_Config::$isSanitized = true;
-    //     // Set 3DS transaction for credit card to true
-    //     Veritrans_Config::$is3ds = true;
-    // }
+    
+    public function paymentgateway()
+    {
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => rand(),
+                'gross_amount' => 10000,
+            )
+        );
+        
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+       return view('bayar',['snapToken'=>$snapToken]);
+    }
 }
