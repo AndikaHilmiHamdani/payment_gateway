aa8f9e3f6f46e9e247f3d734191d5927bdc92a71
AndikaHilmiHamdani <andikahilmi7201@gmail.com>
2022-01-31 18:09:38 +0700

    bug fixing item_details and cust_detail

--- app/Http/Controllers/midtransController.php
+++ app/Http/Controllers/midtransController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
 class midtransController extends Controller
 {
-    
+
     public function paymentgateway()
     {
         //call initialize midtrans payment gateway
-        $this -> initMidtrans();
+        $this->initMidtrans();
 
         $params = array(
             'transaction_details' => array(
@@ -17,27 +18,50 @@ class midtransController extends Controller
                 'gross_amount' => 10000,
             ),
 
-            //quantity error
-            // 'item_details'=>array(
-            //     "id" => "ITEM1",
-            //     "price"=>10000,
-            //     "quantity"=>1,
-            //     "name"=>"abc",
-            //     "brand"=>"def",
-            //     "kategory"=>"ghi",
-            //     "merchant_name"=>"jkl",
-            // ),
+            'item_details' => array(
+                array(
+                    'id' => 'ITEM1',
+                    'price' => 10000,
+                    'quantity' => 1,
+                    'name' => 'abc',
+                    'brand' => 'def',
+                    'categoty' => 'ghi',
+                    'merchant_name' => 'jkl',
+                )
+            ),
 
-            //firstname can't show up
-            'customer_details'=>array(
-                "firstname" => "hamdani",
-                "last_name"=>"dika",
-                "email"=>"abc@abc.com",
-                "phone"=>"+6213456"
-            )            
+            'customer_details' => array(
+                'first_name' => 'dobleh',
+                'last_name' => 'jamal',
+                'email' => 'abc@abc.com',
+                'phone' => '+6213456',
+
+                'billing_address' => array(
+                    'first_name' => 'dobleh',
+                    'last_name' => 'jamal',
+                    'email' => 'abc@abc.com',
+                    'phone' => '+6213456',
+                    'address' => 'aaa',
+                    'city' => 'bbb',
+                    'postal_code' => '123',
+                    'country_code' => 'IDN'
+                ),
+
+                'shipping_address' => array(
+                    'first_name' => 'dobleh',
+                    'last_name' => 'jamal',
+                    'email' => 'abc@abc.com',
+                    'phone' => '+6213456',
+                    'address' => 'aaa',
+                    'city' => 'bbb',
+                    'postal_code' => '123',
+                    'country_code' => 'IDN'
+                )
+                
+            )
         );
         
         $snapToken = \Midtrans\Snap::getSnapToken($params);
-       return view('bayar',['snapToken'=>$snapToken]);
+        return view('bayar', ['snapToken' => $snapToken]);
     }
 }
